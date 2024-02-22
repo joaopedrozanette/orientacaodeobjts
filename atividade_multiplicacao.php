da644a0ccc3daf8e81925bcfb254f60cb79a93e2
João Pedro <135861929+joaopedrozanette@users.noreply.github.com>
2024-02-22 20:08:02 -0300

    atividade_multiplicacao.php

---
+++ atividade_multiplicacao.php
@@ -0,0 +1,12 @@
+<?php 
+
+$x = readline("Digite um número para a multiplacão: ");
+$y = readline("Digite mais outro número para a multiplacão: ");
+$w = 0;
+     for($z = 0; $z < $y;$z++){
+ 
+        $w = $x + $w; 
+ 
+ }
+
+        echo "Resultado: $w";
